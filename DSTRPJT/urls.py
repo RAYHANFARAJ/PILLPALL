1e82403341b5edcba5d10c6a2b2315468143be67
RAYHANFARAJ <rayhan.faraj.23@ump.ac.ma>
2025-01-26 23:03:57 +0100

    Update urls.py

--- DSTRPJT/urls.py
+++ DSTRPJT/urls.py
@@ -1,20 +1,16 @@
-from django.contrib import admin
 from django.urls import path
-from DSTRPJT import views
-
-
-
-
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home, name='home'),  # Page d'accueil
-    path('dashboard/', views.dashboard, name='dashboard'),  # Tableau de bord
-    path('add-medicine/', views.add_medicine, name='add_medicine'),  # Ajouter un médicament
-    path('medicine-list/', views.medicine_list, name='medicine_list'),  # Liste des médicaments
-    path('dispense-medicine/', views.dispense_medicine, name='dispense_medicine'),  # Distribuer un médicament
-    path('login/', views.login_view, name='login'),  # Vue pour la connexion
-    path('signup/', views.signup_view, name='signup'),  # Vue pour l'inscription
+    path('', views.home, name='home'),  # Route pour la page d'accueil
     path('home_authenticated/', views.home_authenticated, name='home_authenticated'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('add-medicine/', views.add_medicine, name='add_medicine'),
+    path('medicine-list/', views.medicine_list, name='medicine_list'),
+    path('dispense_medicine/', views.dispense_medicine, name='dispense_medicine'),
 
+    path('login/', views.login_view, name='login'),
+    path('signup/', views.signup_view, name='signup'),
+    path('etat-eau/', views.etat_eau, name='etat_eau'),
+    path('test/', views.test_view, name='test'),
 ]
